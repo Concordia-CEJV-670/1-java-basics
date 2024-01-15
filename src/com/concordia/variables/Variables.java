bb515cb4c2d1a6d6e218c2aaecbc6adb23359e20
PranavBhatia <bhatiapranav1996@gmail.com>
2024-01-14 20:15:05 -0500

    Add Initial Project

---
+++ src/com/concordia/variables/Variables.java
@@ -0,0 +1,12 @@
+package com.concordia.variables;
+
+public class Variables {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int age = 20;
+		int myAgeNextYear = age + 1;
+		System.out.println(myAgeNextYear);
+	}
+
+}

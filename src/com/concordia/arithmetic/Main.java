cd0d5d9c21501a370c8c5c3c5769e618cd9b5c8b
PranavBhatia <bhatiapranav1996@gmail.com>
2024-01-16 14:10:15 -0500

    basic math

---
+++ src/com/concordia/arithmetic/Main.java
@@ -0,0 +1,21 @@
+package com.concordia.arithmetic;
+
+public class Main {
+
+	public static void main(String[] args) {
+		int result1 = 10 / 3;
+		System.out.println(result1);
+
+		double result2 = 10.0 / 3.0;
+		System.out.println(result2);
+
+		int x = 10;
+		x++;
+		System.out.println(x);
+		System.out.println(x++);
+		System.out.println(++x);
+
+		System.out.println(10 + 3 * 2);
+	}
+
+}

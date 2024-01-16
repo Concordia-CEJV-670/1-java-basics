cc56cd0d16c3cd5d2523c635b65672f213c390ee
PranavBhatia <bhatiapranav1996@gmail.com>
2024-01-16 14:24:26 -0500

    Math class

---
+++ src/com/concordia/math/Main.java
@@ -0,0 +1,15 @@
+package com.concordia.math;
+
+public class Main {
+
+	public static void main(String[] args) {
+//		int x = Math.round(1.4F);
+//		int x = (int) Math.ceil(1.4);
+//		int x = (int) Math.floor(1.4);
+//		int x = Math.max(100, 251);
+//		double x = Math.random();
+		int x = (int) Math.round(Math.random() * 100);
+		System.out.println(x);
+	}
+
+}
